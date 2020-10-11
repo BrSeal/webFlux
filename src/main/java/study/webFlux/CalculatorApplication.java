e4c93b92e405c8b122f3d20aa56dd34874936551
br_se@mail.ru <BronZEseAl132>
2020-10-11 17:53:59 +0300

    Added reactive controller

--- src/main/java/study/webFlux/CalculatorApplication.java
+++ src/main/java/study/webFlux/CalculatorApplication.java
@@ -3,7 +3,7 @@ package study.webFlux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"config","handlers"})
+@SpringBootApplication
 public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
